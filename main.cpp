ac13424bd7a3e56e9f7b8a060af3118cd8b27358
SaumonDeLuxe <planque.adam@gmail.com>
2025-06-03 20:10:41 +0200

    📦 NEW: Add initial project files and implement number guessing game

--- main.cpp
+++ main.cpp
@@ -1,14 +1,51 @@
 #include <iostream>
 
+int main()
+{
 
-int main() {
+    std::string welcometext = "Bienvenue dans le jeu du Nombre Mystère !\n"
+                              "Devinez un nombre entre 1 et 100 :\n";
 
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    int mysteryNumber = std::rand() % 100 + 1;
+    int guess = 0;
+    int attempts = 0;
 
-    for (int i = 1; i <= 5; i++) {
-        std::cout << "i = " << i << std::endl;
+    std::string ggtext = "Bravo ! Vous avez trouvé en %d essais.\n", attemps;
+    std::string tooLowText = "→ Trop petit !\n";
+    std::string tooHighText = "→ Trop grand !\n";
+    std::string guessText = "Votre proposition : ";
+    std::string errorText = "Erreur !\n";
+
+    std::cout << welcometext;
+    std::cin >> guess;
+
+    std::cout << guessText;
+    std::cout << guess;
+
+    while (guess != mysteryNumber)
+    {
+
+        attempts++;
+        if (guess > 100 || guess < 1)
+        {
+            std::cout << errorText;
+            return 1;
+        }
+
+        if (guess > mysteryNumber)
+        {
+            std::cout << tooHighText;
+        }
+        else if (guess < mysteryNumber)
+        {
+            std::cout << tooLowText;
+        }
+
+        std::cout << guessText;
+        std::cin >> guess;
     }
+
+    std::cout << ggtext;
 
     return 0;
 }
